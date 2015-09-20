dd06b5a0385acc2c3829de6283e771f722077c53
ray17n <ramonpujolcots@gmail.com>
2015-09-20 11:48:08 -0400

    reservations. added default deposit functionality

--- templates/node--sr-reservation.tpl.php
+++ templates/node--sr-reservation.tpl.php
@@ -101,7 +101,9 @@
   <?php print render($content['smres_contact_tax_identifier']); ?>
   <?php print render($content['smres_comments']); ?>
   <?php // print render($content['smres_reservation_status']); ?>
-  <?php // print render($content['smres_total_amount']); ?>
-  <?php print $reservation_detail; ?>
+  <?php  print render($content['smres_total_amount']); ?>
+  <?php  print render($content['smres_deposit_amount']); ?>
+  <?php  print render($content['smres_deposit_instructions']); ?>
+  <?php  print $reservation_detail; ?>
   <?php //print render($content); ?>
 </div>
